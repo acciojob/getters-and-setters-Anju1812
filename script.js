8700f5e7aa8120841196ecbcf4a28df98d48aadb
Anju1812 <117847428+Anju1812@users.noreply.github.com>
2023-07-22 19:31:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ class Student extends Person {
   }
 
   study() {
-    console.log(`${this._name} is studying.`);
+    console.log(`${this._name} is studying`);
   }
 }
 
@@ -33,7 +33,7 @@ class Teacher extends Person {
   }
 
   teach() {
-    console.log(`${this._name} is teaching.`);
+    console.log(`${this._name} is teaching`);
   }
 }
 
